838fdac74ffe4c162537ca646d33833b05ffd8ef
Danysan2 <ingdanielcristancho@gmail.com>
2026-04-30 20:09:36 -0500

    fix: agregar --skip-generate en start.sh para evitar error de permisos en runner

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "▶ Running Prisma migrations..."
-node node_modules/prisma/build/index.js db push --schema=./prisma/schema.prisma --accept-data-loss
+node node_modules/prisma/build/index.js db push --schema=./prisma/schema.prisma --accept-data-loss --skip-generate
 
 echo "▶ Starting Next.js..."
 exec node server.js
